3a26c57ca00c11c1108ca1efe5e68f28858a3773
Felipe Meireles <145486045+FELIPE-MEIRELES@users.noreply.github.com>
2023-10-14 21:37:46 -0300

    Delete JavaScript directory

--- JavaScript/Aula 01 como Aula 01-Variáveis e Operadores/01-olamundo.js
+++
@@ -1,2 +0,0 @@
-console.log('Olá, Mundo!');
-//O Nome do Arquevo não pode está separado. Exemplo: "01 - ola mundo!.js"
--- JavaScript/Aula 02 Condicionais/01-resto.js
+++
@@ -1,4 +0,0 @@
-// "%" Pega o resto da divisão
-const numero = 5;
-const resto = numero % 2;
-console.log(resto)
